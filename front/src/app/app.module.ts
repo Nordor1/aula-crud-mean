cc768d10da8e596f548d07cf5a7c16de99342d9b
Felipe Carvalho <feliped3carvalho@gmail.com>
2019-03-03 18:31:43 -0300

    instalado  ng2-slim-loading-bar,  rxjs-compat e importados

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FuncAddComponent } from './func-add/func-add.component';
 import { FuncEditComponent } from './func-edit/func-edit.component';
 import { FuncGetComponent } from './func-get/func-get.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { FuncGetComponent } from './func-get/func-get.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
