023691dd2b6a8ddbd2ccd7345ea3a34924098d25
Felipe Carvalho <feliped3carvalho@gmail.com>
2019-03-13 23:41:26 -0300

    Form validation

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FuncAddComponent } from './func-add/func-add.component';
 import { FuncEditComponent } from './func-edit/func-edit.component';
 import { FuncGetComponent } from './func-get/func-get.component';
-import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,8 @@ import { from } from 'rxjs';
     BrowserModule,
     AppRoutingModule,
     SlimLoadingBarModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/func-add/func-add.component.ts
+++ front/src/app/func-add/func-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-func-add',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncAddComponent implements OnInit {
 
-  constructor() { }
+  adicionarFuncionarioForm: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) {
+    this.createForm();
+  }
+
+  /**
+   * Método responsável por tratar as validações
+   * do Form que criará um novo Funcionario
+   */
+  createForm() {
+    this.adicionarFuncionarioForm = this.formbuilder.group({
+      nomeFuncionario: ['', Validators.required],
+      cargo: ['', Validators.required],
+      numeroidentificador: ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
